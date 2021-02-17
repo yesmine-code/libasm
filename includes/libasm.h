81e72f06c49dbc23a8c6f814c34bee48cf0ff007
ybesbes <ybesbes@student.42.fr>
2021-02-18 00:20:13 +0100

    header file fix

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,12 +6,13 @@
 # include <string.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <errno.h>
 
-ssize_t		ft_write(int fd, char *buffer, size_t size);
-ssize_t		ft_read(int fd, char *buffer, size_t size);
-size_t		ft_strlen(char *str);
-char		*ft_strcpy(char *dest, char *src);
-int			ft_strcmp(char *src1, char *src2);
-char		*ft_strdup(char *str);
+ssize_t		ft_write(int fd, const void *buffer, size_t size);
+ssize_t		ft_read(int fd, void *buffer, size_t size);
+size_t		ft_strlen(const char *s);
+char		*ft_strcpy(char *dest, const char *src);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strdup(const char *s);
 
 #endif
